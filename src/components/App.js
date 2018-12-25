c49a26f4204780193823f71261c7536a18995f98
huyld <huyld@users.noreply.github.com>
2018-12-25 12:01:42 +0700

    feat(App): `App` component ✨

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import 'src/styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Xpense App Client!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
