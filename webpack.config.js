b115018ddd8a5c4316a9b7ba7c32d0010ae0f0b3
huyld <huyld@users.noreply.github.com>
2018-12-28 16:19:19 +0700

    chore(webpack): loaders for image files 🔧

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,25 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jp(e*)g)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.(svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
+      },
     ],
   },
   devtool: 'source-map',
