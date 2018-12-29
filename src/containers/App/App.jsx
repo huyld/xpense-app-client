587f775220da4b94ce7345795f47d1d8e2b3b23a
huyld <huyld@users.noreply.github.com>
2018-12-29 17:04:48 +0700

    feat(App): navigation bar and routing to Home screen ✨

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+import Routes from 'src/Routes';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <h1>Xpense App Client!</h1>
+      <div className="App container">
+        <BrowserRouter>
+          <>
+            <Navbar fluid collapseOnSelect>
+              <Navbar.Header>
+                <Navbar.Brand>
+                  <Link to="/">Xpense</Link>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+              </Navbar.Header>
+            </Navbar>
+            <Routes />
+          </>
+        </BrowserRouter>
       </div>
     );
   }
