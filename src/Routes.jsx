1e7bbcfe36e4663e5147c782289eb9af61f11ad1
huyld <huyld@users.noreply.github.com>
2018-12-29 18:28:05 +0700

    feat(NotFound): add NotFound (404) screen ✨

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from 'src/containers/Home/Home';
+import NotFound from 'src/containers/NotFound/NotFound';
 
 function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
