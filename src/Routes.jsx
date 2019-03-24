d6430408ce37a7d703f4124ec8d12ed104735d04
huyld <huyld@users.noreply.github.com>
2019-03-24 13:01:52 +0700

    feat(Routes): pass childProps to containers ➕️

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 
+import AppliedRoute from 'src/components/AppliedRoute';
 import Home from 'src/containers/Home/Home';
 import NotFound from 'src/containers/NotFound/NotFound';
 
-function Routes() {
+function Routes(props) {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <AppliedRoute path="/" exact component={Home} props={props.childProps} />
       <Route component={NotFound} />
     </Switch>
   );
 }
+
+Routes.propTypes = {
+  childProps: PropTypes.object
+};
 
 export default Routes;
