13909bd61c764848cac442825ae0f53287ca0b85
huyld <huyld@users.noreply.github.com>
2019-04-14 10:36:57 +0700

    feat: add currency list ✨️

---
+++ src/config.js
@@ -0,0 +1,549 @@
+export default {
+  currencies: [
+    {
+      'name': 'Albania Lek',
+      'code': 'ALL',
+      'unicode': '76,101,107'
+    },
+    {
+      'name': 'Afghanistan Afghani',
+      'code': 'AFN',
+      'unicode': '1547'
+    },
+    {
+      'name': 'Argentina Peso',
+      'code': 'ARS',
+      'unicode': '36'
+    },
+    {
+      'name': 'Aruba Guilder',
+      'code': 'AWG',
+      'unicode': '402'
+    },
+    {
+      'name': 'Australia Dollar',
+      'code': 'AUD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Azerbaijan Manat',
+      'code': 'AZN',
+      'unicode': '8380'
+    },
+    {
+      'name': 'Bahamas Dollar',
+      'code': 'BSD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Barbados Dollar',
+      'code': 'BBD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Belarus Ruble',
+      'code': 'BYN',
+      'unicode': '66,114'
+    },
+    {
+      'name': 'Belize Dollar',
+      'code': 'BZD',
+      'unicode': '66,90,36'
+    },
+    {
+      'name': 'Bermuda Dollar',
+      'code': 'BMD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Bolivia Bolíviano',
+      'code': 'BOB',
+      'unicode': '36,98'
+    },
+    {
+      'name': 'Bosnia and Herzegovina Convertible Marka',
+      'code': 'BAM',
+      'unicode': '75,77'
+    },
+    {
+      'name': 'Botswana Pula',
+      'code': 'BWP',
+      'unicode': '80'
+    },
+    {
+      'name': 'Bulgaria Lev',
+      'code': 'BGN',
+      'unicode': '1083,1074'
+    },
+    {
+      'name': 'Brazil Real',
+      'code': 'BRL',
+      'unicode': '82,36'
+    },
+    {
+      'name': 'Brunei Darussalam Dollar',
+      'code': 'BND',
+      'unicode': '36'
+    },
+    {
+      'name': 'Cambodia Riel',
+      'code': 'KHR',
+      'unicode': '6107'
+    },
+    {
+      'name': 'Canada Dollar',
+      'code': 'CAD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Cayman Islands Dollar',
+      'code': 'KYD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Chile Peso',
+      'code': 'CLP',
+      'unicode': '36'
+    },
+    {
+      'name': 'China Yuan Renminbi',
+      'code': 'CNY',
+      'unicode': '165'
+    },
+    {
+      'name': 'Colombia Peso',
+      'code': 'COP',
+      'unicode': '36'
+    },
+    {
+      'name': 'Costa Rica Colon',
+      'code': 'CRC',
+      'unicode': '8353'
+    },
+    {
+      'name': 'Croatia Kuna',
+      'code': 'HRK',
+      'unicode': '107,110'
+    },
+    {
+      'name': 'Cuba Peso',
+      'code': 'CUP',
+      'unicode': '8369'
+    },
+    {
+      'name': 'Czech Republic Koruna',
+      'code': 'CZK',
+      'unicode': '75,269'
+    },
+    {
+      'name': 'Denmark Krone',
+      'code': 'DKK',
+      'unicode': '107,114'
+    },
+    {
+      'name': 'Dominican Republic Peso',
+      'code': 'DOP',
+      'unicode': '82,68,36'
+    },
+    {
+      'name': 'East Caribbean Dollar',
+      'code': 'XCD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Egypt Pound',
+      'code': 'EGP',
+      'unicode': '163'
+    },
+    {
+      'name': 'El Salvador Colon',
+      'code': 'SVC',
+      'unicode': '36'
+    },
+    {
+      'name': 'Euro Member Countries',
+      'code': 'EUR',
+      'unicode': '8364'
+    },
+    {
+      'name': 'Falkland Islands (Malvinas) Pound',
+      'code': 'FKP',
+      'unicode': '163'
+    },
+    {
+      'name': 'Fiji Dollar',
+      'code': 'FJD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Ghana Cedi',
+      'code': 'GHS',
+      'unicode': '162'
+    },
+    {
+      'name': 'Gibraltar Pound',
+      'code': 'GIP',
+      'unicode': '163'
+    },
+    {
+      'name': 'Guatemala Quetzal',
+      'code': 'GTQ',
+      'unicode': '81'
+    },
+    {
+      'name': 'Guernsey Pound',
+      'code': 'GGP',
+      'unicode': '163'
+    },
+    {
+      'name': 'Guyana Dollar',
+      'code': 'GYD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Honduras Lempira',
+      'code': 'HNL',
+      'unicode': '76'
+    },
+    {
+      'name': 'Hong Kong Dollar',
+      'code': 'HKD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Hungary Forint',
+      'code': 'HUF',
+      'unicode': '70,116'
+    },
+    {
+      'name': 'Iceland Krona',
+      'code': 'ISK',
+      'unicode': '107,114'
+    },
+    {
+      'name': 'India Rupee',
+      'code': 'INR',
+      'unicode': ''
+    },
+    {
+      'name': 'Indonesia Rupiah',
+      'code': 'IDR',
+      'unicode': '82,112'
+    },
+    {
+      'name': 'Iran Rial',
+      'code': 'IRR',
+      'unicode': '65020'
+    },
+    {
+      'name': 'Isle of Man Pound',
+      'code': 'IMP',
+      'unicode': '163'
+    },
+    {
+      'name': 'Israel Shekel',
+      'code': 'ILS',
+      'unicode': '8362'
+    },
+    {
+      'name': 'Jamaica Dollar',
+      'code': 'JMD',
+      'unicode': '74,36'
+    },
+    {
+      'name': 'Japan Yen',
+      'code': 'JPY',
+      'unicode': '165'
+    },
+    {
+      'name': 'Jersey Pound',
+      'code': 'JEP',
+      'unicode': '163'
+    },
+    {
+      'name': 'Kazakhstan Tenge',
+      'code': 'KZT',
+      'unicode': '1083,1074'
+    },
+    {
+      'name': 'Korea (North) Won',
+      'code': 'KPW',
+      'unicode': '8361'
+    },
+    {
+      'name': 'Korea (South) Won',
+      'code': 'KRW',
+      'unicode': '8361'
+    },
+    {
+      'name': 'Kyrgyzstan Som',
+      'code': 'KGS',
+      'unicode': '1083,1074'
+    },
+    {
+      'name': 'Laos Kip',
+      'code': 'LAK',
+      'unicode': '8365'
+    },
+    {
+      'name': 'Lebanon Pound',
+      'code': 'LBP',
+      'unicode': '163'
+    },
+    {
+      'name': 'Liberia Dollar',
+      'code': 'LRD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Macedonia Denar',
+      'code': 'MKD',
+      'unicode': '1076,1077,1085'
+    },
+    {
+      'name': 'Malaysia Ringgit',
+      'code': 'MYR',
+      'unicode': '82,77'
+    },
+    {
+      'name': 'Mauritius Rupee',
+      'code': 'MUR',
+      'unicode': '8360'
+    },
+    {
+      'name': 'Mexico Peso',
+      'code': 'MXN',
+      'unicode': '36'
+    },
+    {
+      'name': 'Mongolia Tughrik',
+      'code': 'MNT',
+      'unicode': '8366'
+    },
+    {
+      'name': 'Mozambique Metical',
+      'code': 'MZN',
+      'unicode': '77,84'
+    },
+    {
+      'name': 'Namibia Dollar',
+      'code': 'NAD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Nepal Rupee',
+      'code': 'NPR',
+      'unicode': '8360'
+    },
+    {
+      'name': 'Netherlands Antilles Guilder',
+      'code': 'ANG',
+      'unicode': '402'
+    },
+    {
+      'name': 'New Zealand Dollar',
+      'code': 'NZD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Nicaragua Cordoba',
+      'code': 'NIO',
+      'unicode': '67,36'
+    },
+    {
+      'name': 'Nigeria Naira',
+      'code': 'NGN',
+      'unicode': '8358'
+    },
+    {
+      'name': 'Norway Krone',
+      'code': 'NOK',
+      'unicode': '107,114'
+    },
+    {
+      'name': 'Oman Rial',
+      'code': 'OMR',
+      'unicode': '65020'
+    },
+    {
+      'name': 'Pakistan Rupee',
+      'code': 'PKR',
+      'unicode': '8360'
+    },
+    {
+      'name': 'Panama Balboa',
+      'code': 'PAB',
+      'unicode': '66,47,46'
+    },
+    {
+      'name': 'Paraguay Guarani',
+      'code': 'PYG',
+      'unicode': '71,115'
+    },
+    {
+      'name': 'Peru Sol',
+      'code': 'PEN',
+      'unicode': '83,47,46'
+    },
+    {
+      'name': 'Philippines Peso',
+      'code': 'PHP',
+      'unicode': '8369'
+    },
+    {
+      'name': 'Poland Zloty',
+      'code': 'PLN',
+      'unicode': '122,322'
+    },
+    {
+      'name': 'Qatar Riyal',
+      'code': 'QAR',
+      'unicode': '65020'
+    },
+    {
+      'name': 'Romania Leu',
+      'code': 'RON',
+      'unicode': '108,101,105'
+    },
+    {
+      'name': 'Russia Ruble',
+      'code': 'RUB',
+      'unicode': '8381'
+    },
+    {
+      'name': 'Saint Helena Pound',
+      'code': 'SHP',
+      'unicode': '163'
+    },
+    {
+      'name': 'Saudi Arabia Riyal',
+      'code': 'SAR',
+      'unicode': '65020'
+    },
+    {
+      'name': 'Serbia Dinar',
+      'code': 'RSD',
+      'unicode': '1044,1080,1085,46'
+    },
+    {
+      'name': 'Seychelles Rupee',
+      'code': 'SCR',
+      'unicode': '8360'
+    },
+    {
+      'name': 'Singapore Dollar',
+      'code': 'SGD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Solomon Islands Dollar',
+      'code': 'SBD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Somalia Shilling',
+      'code': 'SOS',
+      'unicode': '83'
+    },
+    {
+      'name': 'South Africa Rand',
+      'code': 'ZAR',
+      'unicode': '82'
+    },
+    {
+      'name': 'Sri Lanka Rupee',
+      'code': 'LKR',
+      'unicode': '8360'
+    },
+    {
+      'name': 'Sweden Krona',
+      'code': 'SEK',
+      'unicode': '107,114'
+    },
+    {
+      'name': 'Switzerland Franc',
+      'code': 'CHF',
+      'unicode': '67,72,70'
+    },
+    {
+      'name': 'Suriname Dollar',
+      'code': 'SRD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Syria Pound',
+      'code': 'SYP',
+      'unicode': '163'
+    },
+    {
+      'name': 'Taiwan New Dollar',
+      'code': 'TWD',
+      'unicode': '78,84,36'
+    },
+    {
+      'name': 'Thailand Baht',
+      'code': 'THB',
+      'unicode': '3647'
+    },
+    {
+      'name': 'Trinidad and Tobago Dollar',
+      'code': 'TTD',
+      'unicode': '84,84,36'
+    },
+    {
+      'name': 'Turkey Lira',
+      'code': 'TRY',
+      'unicode': ''
+    },
+    {
+      'name': 'Tuvalu Dollar',
+      'code': 'TVD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Ukraine Hryvnia',
+      'code': 'UAH',
+      'unicode': '8372'
+    },
+    {
+      'name': 'United Kingdom Pound',
+      'code': 'GBP',
+      'unicode': '163'
+    },
+    {
+      'name': 'United States Dollar',
+      'code': 'USD',
+      'unicode': '36'
+    },
+    {
+      'name': 'Uruguay Peso',
+      'code': 'UYU',
+      'unicode': '36,85'
+    },
+    {
+      'name': 'Uzbekistan Som',
+      'code': 'UZS',
+      'unicode': '1083,1074'
+    },
+    {
+      'name': 'Venezuela Bolívar',
+      'code': 'VEF',
+      'unicode': '66,115'
+    },
+    {
+      'name': 'Viet Nam Dong',
+      'code': 'VND',
+      'unicode': '8363'
+    },
+    {
+      'name': 'Yemen Rial',
+      'code': 'YER',
+      'unicode': '65020'
+    },
+    {
+      'name': 'Zimbabwe Dollar',
+      'code': 'ZWD',
+      'unicode': '90,36'
+    }
+  ]
+};
