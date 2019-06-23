10d7a2e7f86e06a008f29c1750a2ac68f322f9dd
huyld <huyld@users.noreply.github.com>
2019-06-23 15:43:45 +0700

    fix: errors occur after upgrading dependencies 🐛️

--- src/components/LoaderButton/LoaderButton.jsx
+++ src/components/LoaderButton/LoaderButton.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import Octicon, { Sync } from '@primer/octicons-react';
 
 /**
  * Button with spinning icon
  */
 // eslint-disable-next-line react/display-name
-export default ({
+const LoaderButton = ({
   isLoading,
   text,
   loadingText,
@@ -22,3 +23,13 @@ export default ({
     {isLoading && <Octicon icon={Sync} className="spinning" />}
     {!isLoading ? text : loadingText}
   </Button>;
+
+LoaderButton.propTypes = {
+  isLoading: PropTypes.bool,
+  text: PropTypes.string,
+  loadingText: PropTypes.string,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+export default LoaderButton;
